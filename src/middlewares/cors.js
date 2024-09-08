5a84169d800875ee29eb2952ec1c59abe0afe9f6
JoelSalum <joelsalum.dev@gmail.com>
2024-09-08 19:47:48 -0300

    problema con Cors en render

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -3,11 +3,7 @@ import cors from 'cors'
 export const corsMiddleware = () => cors({
     origin: (origin, callback) => {
         const ACCEPTED_ORIGINS = [
-            'http://localhost:8080',
-            'http://localhost:1234',
-            'http://127.0.0.1:5500',
-            "http://localhost:3000",
-            "http://localhost:1235"
+            '*'
         ]
 
         if(ACCEPTED_ORIGINS.includes(origin)) {
