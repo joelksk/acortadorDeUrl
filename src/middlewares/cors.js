598b0697e382c914a9325b58108ddfef4ae77042
JoelSalum <joelsalum.dev@gmail.com>
2024-09-13 10:36:24 -0300

    Agregamos aal middleware de cors una variable de entorno.

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,10 +1,12 @@
 import cors from 'cors'
+import 'dotenv/config'
 
 export const corsMiddleware = () => cors({
     origin: (origin, callback) => {
         const ACCEPTED_ORIGINS = [
             'http://localhost:1235',
-            'http://localhost:3000'
+            'http://localhost:3000',
+            process.env.DOMAIN_CORS
         ]
 
         if(ACCEPTED_ORIGINS.includes(origin)) {
